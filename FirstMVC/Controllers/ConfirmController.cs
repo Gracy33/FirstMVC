8812db672557e813a7fc92e5ca9508c62852fdb0
Gracy33 <gracemingiedi@gmail.com>
2014-10-07 16:47:07 +0200

    Fin cours 07/10

---
+++ FirstMVC/Controllers/ConfirmController.cs
@@ -0,0 +1,31 @@
+﻿using FirstMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVC.Controllers
+{
+    public class ConfirmController : Controller
+    {
+        //
+        // GET: /Confirm/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult confirm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult confirm(Response response)
+        {
+            return View("Thanx", response);
+        }
+	}
+}
---
+++ FirstMVC/Models/Response.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FirstMVC.Models
+{
+    public class Response
+    {
+        [Required(ErrorMessage = "Veuillez renseigner votre nom")]
+        public string Nom { get; set; }
+        [Required(ErrorMessage = "Veuillez renseigner votre mail")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Veuillez renseigner votre Téléphone")]
+        public string Telephone { get; set; }
+        [Required(ErrorMessage = "Veuillez nous informer votre présence")]
+        public bool? presence { get; set; }
+    }
+}
